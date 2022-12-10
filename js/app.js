26090f9b9ef2f8e0180014c2f28a7cf4c8282e21
sjh0020 <1977685805@qq.com>
2022-12-10 19:14:17 +0800

    v0.3.1 fix the bug

--- js/app.js
+++ js/app.js
@@ -1,13 +1,50 @@
 // import {getData as getDataOfYBZC} from 'https://cdn.jsdelivr.net/gh/sjh0020/pythonClassDemo@master/js/各省医保支出.js';
+// import { getData as getDataOfZRK } from 'https://cdn.jsdelivr.net/gh/sjh0020/pythonClassDemo@master/js/各省总人口.js';
+// import { getData as getDataOfWSRS } from 'https://cdn.jsdelivr.net/gh/sjh0020/pythonClassDemo@master/js/各省卫生人数.js';
 import {getData as getDataOfYBZC} from './各省医保支出.js';
 import { getData as getDataOfZRK } from './各省总人口.js';
-import { getData as getDataOfWSRS } from './各省医师数.js';
+import { getData as getDataOfWSRS } from './各省卫生人数.js';
 
-document.querySelector('#y2021').onclick = changeOption('zrk');
+window.onload = eventHandle(2021);
+
+document.getElementById('y2021').addEventListener('click', () => {eventHandle(2021);});
+document.getElementById('y2020').addEventListener('click', () => {eventHandle(2020);});
+document.getElementById('y2019').addEventListener('click', () => {eventHandle(2019);});
+document.getElementById('y2018').addEventListener('click', () => {eventHandle(2018);});
+document.getElementById('y2017').addEventListener('click', () => {eventHandle(2017);});
+document.getElementById('y2016').addEventListener('click', () => {eventHandle(2016);});
+document.getElementById('y2015').addEventListener('click', () => {eventHandle(2015);});
+document.getElementById('y2014').addEventListener('click', () => {eventHandle(2014);});
+document.getElementById('y2013').addEventListener('click', () => {eventHandle(2013);});
+document.getElementById('y2012').addEventListener('click', () => {eventHandle(2012);});
+document.getElementById('y2011').addEventListener('click', () => {eventHandle(2011);});
+document.getElementById('y2010').addEventListener('click', () => {eventHandle(2010);});
+document.getElementById('y2009').addEventListener('click', () => {eventHandle(2009);});
+document.getElementById('y2008').addEventListener('click', () => {eventHandle(2008);});
+document.getElementById('y2007').addEventListener('click', () => {eventHandle(2007);});
+document.getElementById('y2006').addEventListener('click', () => {eventHandle(2006);});
+document.getElementById('y2005').addEventListener('click', () => {eventHandle(2005);});
+document.getElementById('y2004').addEventListener('click', () => {eventHandle(2004);});
+document.getElementById('y2003').addEventListener('click', () => {eventHandle(2003);});
+document.getElementById('y2002').addEventListener('click', () => {eventHandle(2002);});
 
 document.querySelector('#ybzc').addEventListener('click', () => {changeOption('ybzc')});
 document.querySelector('#wsrs').addEventListener('click', () => {changeOption('wsrs')});
 document.querySelector('#zrk').addEventListener('click', () => {changeOption('zrk')});
+
+function eventHandle(y) {
+    var option_now = document.getElementsByClassName('nowOptional')[0].id;
+    if (option_now == 'ybzc') {
+        getDataOfYBZC(y);
+        changeYear(`y${y}`);
+    } else if (option_now == 'zrk') {
+        getDataOfZRK(y);
+        changeYear(`y${y}`);
+    } else if (option_now == 'wsrs') {
+        getDataOfWSRS(y);
+        changeYear(`y${y}`);
+    }
+}
 
 function changeYear(id) {
     var class_now = document.getElementsByClassName('nowYear');
@@ -21,43 +58,6 @@ function changeOption(id) {
     class_now[0].classList.remove('nowOptional');
     var add_class = document.getElementById(id);
     add_class.classList.add('nowOptional');
-    var nowOptionId = id;
-    if (nowOptionId == 'ybzc') {
-        document.querySelector('#y2021').onclick = getDataOfYBZC(2021);
-        changeYear('y2021')
-        var lists = document.getElementsByClassName('y');
-        for (var i = 0; i < lists.length; i++) {
-            (function() {
-                var id = lists[i].id;
-                var y = lists[i].innerHTML;
-                lists[i].addEventListener('click', () => {getDataOfYBZC(y);changeYear(id);});
-            })(i);
-        }
-    };
-    
-    if (nowOptionId == 'zrk') {
-        document.querySelector('#y2021').onclick = getDataOfZRK(2021);
-        changeYear('y2021')
-        var lists = document.getElementsByClassName('y');
-        for (var i = 0; i < lists.length; i++) {
-            (function() {
-                var id = lists[i].id;
-                var y = lists[i].innerHTML;
-                lists[i].addEventListener('click', () => {getDataOfZRK(y);changeYear(id);});
-            })(i);
-        }
-    };
-
-    if (nowOptionId == 'wsrs') {
-        document.querySelector('#y2021').onclick = getDataOfWSRS(2021);
-        changeYear('y2021')
-        var lists = document.getElementsByClassName('y');
-        for (var i = 0; i < lists.length; i++) {
-            (function() {
-                var id = lists[i].id;
-                var y = lists[i].innerHTML;
-                lists[i].addEventListener('click', () => {getDataOfWSRS(y);changeYear(id);});
-            })(i);
-        }
-    };
+    var year_now = document.getElementsByClassName('nowYear')[0];
+    eventHandle(year_now.innerHTML);
 }
